1b900aa21b4cb5509e9a446dcd23e02afa57bd3b
Pavel Koryakin <unclekoin@gmail.com>
2021-06-19 17:02:22 +0300

    feat: add structure for events

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,15 @@
 import {DOMListener} from './DOMListener';
 
 export class ExcelComponent extends DOMListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
   // Returns a component template
   toHTML() {
     return '';
+  }
+
+  init() {
+    this.initDOMListeners();
   }
 }
