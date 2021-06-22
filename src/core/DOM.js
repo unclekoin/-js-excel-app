b11e241eb417ad59a45ec0a8e487c1dbc44dffb0
Pavel Koryakin <unclekoin@gmail.com>
2021-06-22 11:06:10 +0300

    feat: create class DOMListener

--- src/core/DOM.js
+++ src/core/DOM.js
@@ -22,6 +22,10 @@ class DOM {
     this.$el.addEventListener(eventType, callback);
   }
 
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback);
+  }
+
   append(node) {
     if (node instanceof DOM) {
       node = node.$el;
